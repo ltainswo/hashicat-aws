67b6f2be4cc30a20d3fafb4150dbf1fb3d19d1cd
Taylor Ainsworth <ltainswo@amazon.com>
2023-10-10 01:15:01 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance",
-    Deparment = "devops"
+    Deparment = "devops",
+    Billable = "true"
   }
 }
 
